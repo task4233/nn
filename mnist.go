4899b69bbb3a756c2a878bf7f0f366ecafc2c3d2
task4233 <tks.m1205@gmail.com>
2020-02-16 12:22:52 +0900

    save 2/16

--- mnist.go
+++ mnist.go
@@ -15,48 +15,67 @@ type mnist struct {
 
 type mnist_label struct {
 	filePath    string
-	numOfImages int
+	numOfLabels int
 	data        []int
 }
 
 type mnist_data struct {
 	filePath    string
 	numOfImages int
-    rows int
-    columns int
-	data        [][][]int
+	rows        int
+	columns     int
+	// datas HxW(3-dimension)
+	data [][][]byte
 }
 
 func (m *mnist) load_datas() error {
+    fmt.Println("read training images")
 	if err := m.train_images.load_data(); err != nil {
 		return fmt.Errorf("train_images : %v\n", err)
 	}
 
-	if err := m.train_labels.load_data(); err != nil {
+    fmt.Println("read training labels")
+	if err := m.train_labels.load_label(); err != nil {
 		return fmt.Errorf("train_labels : %v\n", err)
 	}
 
+    fmt.Println("read test images")
 	if err := m.test_images.load_data(); err != nil {
 		return fmt.Errorf("train_images : %v\n", err)
 	}
 
-	if err := m.test_labels.load_data(); err != nil {
+    fmt.Println("read test labels")
+	if err := m.test_labels.load_label(); err != nil {
 		return fmt.Errorf("train_labels : %v\n", err)
 	}
 	return nil
 }
 
+// [offset] [type]          [value]          [description]
+// 0000     32 bit integer  0x00000801(2049) magic number (MSB first)
+// 0004     32 bit integer  60000            number of items
+// 0008     unsigned byte   ??               label
+// 0009     unsigned byte   ??               label
+// ........
+// xxxx     unsigned byte   ??               label
+// The labels values are 0 to 9.
 func (m *mnist_label) load_label() error {
-    data, err := ioutil.ReadFile(m.filePath)
-    if err != nil {
-        return err
-    }
+	data, err := ioutil.ReadFile(m.filePath)
+	if err != nil {
+		return err
+	}
+    
+	// the number of labels are in data[4:8]
+	m.numOfLabels = int(binary.BigEndian.Uint32(data[4:8]))
+    m.data = make([]int, m.numOfLabels)
 
-    // the number of images are in data[4:8]
-    m.numOfImages = int(binary.BigEndian.Uint32(data[4:8]))
-    
+	for idx := 0; idx < m.numOfLabels; idx++ {
+        fmt.Println(idx)
+        fmt.Println(int(binary.BigEndian.Uint32(data[8+idx : 8+(idx+1)*4])))
+		m.data[idx] = int(binary.BigEndian.Uint32(data[8+idx : 8+(idx+1)]))
+	}
+	return nil
 }
-    
 
 // load_data loads image data
 // TRAINING SET IMAGE FILE (train-images-idx3-ubyte):
@@ -75,21 +94,30 @@ func (m *mnist_data) load_data() error {
 		return err
 	}
 
+    // initializing
 	// the number of images are in data[4:8]
 	m.numOfImages = int(binary.BigEndian.Uint32(data[4:8]))
-    m.rows = int(binary.BigEndian.Uint32(data[8:12]))
-    m.columns = int(binary.BigEndian.Uint32(data[12:16]))
-
-    sq := m.rows * m.columns
+    m.data = make([][][]byte, m.numOfImages)
     
-    for idx:= 0; idx < m.numOfImages; idx++ {
-        for ri := 0; ri < rows; ri++ {
-            // TODO
-            m.data[idx][ri] := data[8 + idx * sq: 8 + (idx + 1) * sq]
+	m.rows = int(binary.BigEndian.Uint32(data[8:12]))
+    for idx:=0; idx<m.numOfImages; idx++ {
+        m.data[idx] = make([][]byte, m.rows)
+    }
+    
+	m.columns = int(binary.BigEndian.Uint32(data[12:16]))
+    for idx :=0; idx<m.numOfImages; idx++ {
+        for ri:=0; ri<m.rows; ri++ {
+            m.data[idx][ri] = make([]byte, m.columns)
         }
     }
-	m.data = data[8:]
-    
 
+	sq := m.rows * m.columns
+
+	for idx := 0; idx < m.numOfImages; idx++ {
+		for ri := 0; ri < m.rows; ri++ {
+			// TODO
+			m.data[idx][ri] = data[8+idx*sq : 8+(idx+1)*sq]
+		}
+	}
 	return nil
 }
--- mnist_test.go
+++ mnist_test.go
@@ -10,22 +10,22 @@ func TestMnist(t *testing.T) {
 		train_images: &mnist_data{
 			filePath:    "./mnist/train-images-idx3-ubyte",
 			numOfImages: 0,
-			data:        []byte{},
+			data:        [][][]byte{},
 		},
-		train_labels: &mnist_data{
+		train_labels: &mnist_label{
 			filePath:    "./mnist/train-images-idx3-ubyte",
-			numOfImages: 0,
-			data:        []byte{},
+			numOfLabels: 0,
+			data:        []int{},
 		},
 		test_images: &mnist_data{
 			filePath:    "./mnist/t10k-images-idx3-ubyte",
 			numOfImages: 0,
-			data:        []byte{},
+			data:        [][][]byte{},
 		},
-		test_labels: &mnist_data{
+		test_labels: &mnist_label{
 			filePath:    "./mnist/t10k-labels-idx1-ubyte",
-			numOfImages: 0,
-			data:        []byte{},
+			numOfLabels: 0,
+			data:        []int{},
 		},
 	}
 
@@ -34,33 +34,33 @@ func TestMnist(t *testing.T) {
 		t.Errorf("Error: %v\n", err)
 	}
 
+	// load data(false)
 	fp := mnists.train_images.filePath
 	mnists.train_images.filePath = ""
-	// load data(false)
 	if err := mnists.load_datas(); err == nil {
 		t.Errorf("Error: %v\n", err)
 	}
 	mnists.train_images.filePath = fp
 
+	// load data(false)
 	fp = mnists.train_labels.filePath
 	mnists.train_labels.filePath = ""
-	// load data(false)
 	if err := mnists.load_datas(); err == nil {
 		t.Errorf("Error: %v\n", err)
 	}
 	mnists.train_labels.filePath = fp
 
+	// load data(false)
 	fp = mnists.test_images.filePath
 	mnists.test_images.filePath = ""
-	// load data(false)
 	if err := mnists.load_datas(); err == nil {
 		t.Errorf("Error: %v\n", err)
 	}
 	mnists.test_images.filePath = fp
 
+	// load data(false)
 	fp = mnists.test_labels.filePath
 	mnists.test_labels.filePath = ""
-	// load data(false)
 	if err := mnists.load_datas(); err == nil {
 		t.Errorf("Error: %v\n", err)
 	}
