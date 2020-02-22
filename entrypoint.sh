67b81f45ad764bb50d18410516a2fcf68da0756f
task4233 <tks.m1205@gmail.com>
2020-02-22 14:16:07 +0900

    update setting files for CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # -------------
 # Environments
-# ------------
+# -------------
 
 RUN=$1
 PKGROOT=$2
@@ -12,16 +12,15 @@ SEND_COMMENT=$3
 COMMENT=""
 SUCCESS=0
 
+# if not set, assign default value
 if [ "$2" = "" ]; then
     PKGROOT="."
 fi
+if [ "$3" = "" ]; then
+    SEND_COMMENT="true"
+fi
 
 PKGNAME=$(go list $PKGROOT/...)
-
-REVIEWDOG_ARG="-reporter='github-pr-review'"
-if [ "$CI_PULL_REQUEST" = "" ]; then
-    REVIEWDOG_ARG="-diff='git diff master'"
-fi
 
 # ------------
 # Functions
@@ -33,6 +32,19 @@ send_comment() {
     PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')
     COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
     curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "{PAYLOAD}" "${COMMENTS_URL}" > /dev/null
+}
+
+module_download() {
+    # if not exist go.mod
+    if [ ! -e go.mod ]; then
+	go mod init
+    fi
+
+    # if finished in error status, exit 1
+    go mod download
+    if [ $? -ne 0 ]; then
+	exit 1;
+    fi
 }
 
 # run_reviewdog executes golint, gsc, staticcheck by reviewdog
@@ -74,5 +86,22 @@ ${OUTPUT}
 # -------------
 # Main
 # ------------
+case ${RUN} in
+    "errcheck" )
+	module_download
+	exec_errcheck
+	;;
+    * )
+	echo "Invalid command." >&2
+	exit 1
+esac
 
-run_reviewdog
+if [ ${SUCCESS} -ne 0 ]; then
+    echo "Check failed." >&2
+    echo "${COMMENT}" >&2
+    if [ "${SEND_COMMENT}" = "true" ]; then
+	send_comment
+    fi
+fi
+
+exit ${SUCCESS}
