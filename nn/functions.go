b11e21825c95719a160c8d448d4a5a1b902cf3c6
task4233 <tks.m1205@gmail.com>
2020-02-23 15:39:51 +0900

    merge operation health

---
+++ nn/functions.go
@@ -0,0 +1,32 @@
+package nn
+
+import "math"
+
+// Sigmoid is an activation function.
+// f(arr[i]) = 1 / (1 + exp(-arr[i]))
+func Sigmoid(arr [][]float64) [][]float64 {
+	resArr := makeMatrix(len(arr), len(arr[0]))
+	for ri := 0; ri < len(arr); ri++ {
+		for ci := 0; ci < len(arr[ri]); ci++ {
+			resArr[ri][ci] = 1.0 / (1.0 + math.Exp(-arr[ri][ci]))
+		}
+	}
+	return resArr
+}
+
+// Softmax is an activation function.
+// see: https://en.wikipedia.org/wiki/Softmax_function
+func Softmax(arr [][]float64) [][]float64 {
+	resArr := makeMatrix(len(arr), len(arr[0]))
+	for ri := 0; ri < len(arr); ri++ {
+		sum := 0.0
+		for ci := 0; ci < len(arr[ri]); ci++ {
+			sum += math.Exp(arr[ri][ci])
+		}
+
+		for ci := 0; ci < len(arr[ri]); ci++ {
+			resArr[ri][ci] = math.Exp(arr[ri][ci]) / sum
+		}
+	}
+	return resArr
+}
---
+++ nn/go.mod
@@ -0,0 +1,5 @@
+module nn/nn
+
+go 1.13
+
+require github.com/securego/gosec v0.0.0-20200203094520-d13bb6d2420c // indirect
